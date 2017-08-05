05fa8b088633bc528b6efa20d4382f1e8313290b
Edwin <populationtwo@gmail.com>
2017-08-04 23:38:41 -0400

    Update text domain.

--- public/class-camden-simple-twitter-public.php
+++ public/class-camden-simple-twitter-public.php
@@ -54,51 +54,6 @@ class Camden_Simple_Twitter_Public {
 
 	}
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Camden_Simple_Twitter_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Camden_Simple_Twitter_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/camden-simple-twitter-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Camden_Simple_Twitter_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Camden_Simple_Twitter_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/camden-simple-twitter-public.js', array( 'jquery' ), $this->version, false );
-
-	}
 
 	/**
 	 * NOTE:  Actions are points in the execution of a page or process
